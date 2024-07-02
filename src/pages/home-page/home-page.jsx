2f1388edc3c08a1753d1484ae60fc80977f05e32
Habeeb <habeebahmadu1@gmail.com>
2024-07-02 23:19:09 +0100

    feat: add hero

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -6,11 +6,13 @@ import Header from '../../components/header/header'
 import Footer from '../../components/footer/footer'
 import PopularPost from '../../components/popular-post/popular-post'
 import RecentPost from '../../components/recent-post/recent-post'
+import Hero from '../../components/hero/hero'
 
 const HomePage = () => {
   return (
     <div>
       <Header/>
+      <Hero/>
       <RecentPost/>
       <PopularPost/>
       <Footer/>
