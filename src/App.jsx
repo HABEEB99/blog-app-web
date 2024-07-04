bb6e3fd33bd0df484cc14bcbb6bc52f13632c269
Habeeb <habeebahmadu1@gmail.com>
2024-07-04 22:24:42 +0100

    feat: setup app router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import HomePage from "./pages/home-page/home-page"
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+import HomePage from "./pages/home-page/home-page";
+import MainLayout from "./layouts/main-layout/main-layout";
+import AboutPage from "./pages/about-page/about-page";
+
+const appRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <div>
-      <HomePage/>
-    </div>
-  )
-}
+  return <RouterProvider router={appRouter} />;
+};
 
-export default App
+export default App;
